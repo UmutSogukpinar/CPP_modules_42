061f96596b5b64e03262f79a8c5ea2841d81a2d8
umutsogukpinar <usogukpinar0@gmail.com>
2025-05-24 19:45:24 +0300

    fix: constructor implemented

--- cpp_01/ex00/Zombie.cpp
+++ cpp_01/ex00/Zombie.cpp
@@ -1,6 +1,17 @@
 #include "Zombie.hpp"
 
 
+Zombie::Zombie(std::string name)
+{
+    name_ = name;
+}
+#
+Zombie::~Zombie()
+{
+	std::cout << name_ << " Destructor called!" << std::endl;
+}
+
+
 void Zombie::announce(void) const
 {
     std::cout << name_ << ": BraiiiiiiinnnzzzZ..." << "\n";
