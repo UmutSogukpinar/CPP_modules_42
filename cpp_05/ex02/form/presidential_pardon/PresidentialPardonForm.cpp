d11c12a326192ea329c4755746f7a32d9623a086
Umut Soğukpınar <usogukpinar0@gmail.com>
2025-08-03 18:31:13 +0300

    feat: cpp_05: ex02: implement presidential_pardon class

---
+++ cpp_05/ex02/form/presidential_pardon/PresidentialPardonForm.cpp
@@ -0,0 +1,57 @@
+#include "Bureaucrat.hpp"
+#include "PresidentialPardonForm.hpp"
+
+// Default Constructor
+PresidentialPardonForm::PresidentialPardonForm()
+    : AForm(PP_NAME, PP_SIGN_GRADE, PP_EXEC_GRADE)
+{
+    std::cout << "Default constructor called for PresidentialPardonForm "
+              << name_ << "\n";
+}
+
+// Parameterized Constructor
+PresidentialPardonForm::PresidentialPardonForm(std::string name)
+    : AForm(name, PP_SIGN_GRADE, PP_EXEC_GRADE)
+{
+    std::cout << "Parameterized constructor called for PresidentialPardonForm "
+              << name_ << "\n";
+}
+
+// Copy Constructor
+PresidentialPardonForm::PresidentialPardonForm(PresidentialPardonForm const &other)
+    : AForm(other.name_, other.signGrade_, other.executeGrade_)
+{
+    std::cout << "Copy constructor called for PresidentialPardonForm: "
+              << name_ << "\n";
+}
+
+// Destructor
+PresidentialPardonForm::~PresidentialPardonForm()
+{
+    std::cout << "Destructor called for PresidentialPardonForm: "
+              << name_ << "\n";
+}
+
+// Operator assignment
+PresidentialPardonForm &PresidentialPardonForm::operator=(PresidentialPardonForm const &other)
+{
+    if (this != &other)
+    {
+        std::cout << "Assignment operator called for PresidentialPardonForm: "
+                  << name_ << "\n";
+        isSigned_ = other.isSigned_;
+    }
+    return (*this);
+}
+
+// ========================== Member Functions ==========================
+
+void PresidentialPardonForm::execute(const Bureaucrat &bureaucrat)
+{
+    if (!this->getIsSigned())
+        throw std::runtime_error("Form must be signed before execution.");
+    if (bureaucrat.getGrade() > this->getExecuteGrade())
+        throw std::runtime_error("Bureaucrat grade too low to execute this form.");
+
+    std::cout << name_ << " has been pardoned by Zaphod Beeblebrox.\n";
+}
---
+++ cpp_05/ex02/form/presidential_pardon/PresidentialPardonForm.hpp
@@ -0,0 +1,35 @@
+#ifndef PRESIDENTIAL_PARDON_FORM_HPP
+# define PRESIDENTIAL_PARDON_FORM_HPP
+
+# include "AForm.hpp"
+
+# define PP_SIGN_GRADE  25
+# define PP_EXEC_GRADE  5
+# define PP_NAME        "PresidentialPardonForm"
+
+
+// TODO: implement custom error classes
+
+class PresidentialPardonForm : public AForm
+{
+    // Default Constructor
+    PresidentialPardonForm();
+
+	// Parameterized Constructor
+    PresidentialPardonForm(std::string name);
+
+    // Copy Constructor
+    PresidentialPardonForm(PresidentialPardonForm const &other);
+
+    // Destructor
+    ~PresidentialPardonForm();
+
+    // Assignment Operator
+    PresidentialPardonForm & operator=(PresidentialPardonForm const &other);
+
+    // Member Functions
+
+	void execute(Bureaucrat const &bureaucrat);
+};
+
+# endif
